57a94fa9716cb320ea80cbedac758a4c7ed3b345
Bagrat <bag.antonyan@gmail.com>
2021-04-21 17:31:32 +0400

    Enter coins

--- CoffeeMachine.Client/Program.cs
+++ CoffeeMachine.Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CoffeeMachine.Client
 {
@@ -6,7 +7,38 @@ namespace CoffeeMachine.Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Execute();
+        }
+
+        static void EnterCoins()
+        {
+            var coins = new List<decimal> { 50, 100, 200, 500 };
+
+            Console.WriteLine("Welcome to coffee machine program!");
+            Console.WriteLine("Please enter coin(s), (50, 100, 200, 500) - only mentioned coins can be entered");
+            Console.WriteLine("if it's enougth just press 'Enter' key");
+            string input;
+            decimal balance = 0;
+            decimal coin;
+
+            do
+            {
+                Console.WriteLine($"Balance - {balance}");
+                Console.Write("===== Insert coin => ");
+                input = Console.ReadLine();
+                bool isNumber = decimal.TryParse(input, out coin);
+
+                if (isNumber && coins.Contains(coin))
+                {
+                    balance += coin;
+                    continue;
+                }
+                else if(!string.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("Sorry, but wrong coin or invalid value has been entered.");
+                    Console.WriteLine("Please try again, or tap 'Enter' key to choose a coffee.");
+                }
+            } while (!string.IsNullOrWhiteSpace(input));
         }
     }
 }
